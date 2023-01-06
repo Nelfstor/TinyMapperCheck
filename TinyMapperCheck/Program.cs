de33bf24c0e0a2fc5fe03780f193c3afa8f8afe3
nelfstor <nelfstor@gmail.com>
2023-01-06 11:54:51 +0400

    Add project files.

---
+++ TinyMapperCheck/Program.cs
@@ -0,0 +1,43 @@
+﻿using Nelibur.ObjectMapper;
+
+public class MyClass
+{
+    public DateTimeOffset SomeDate { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class MyClassDto
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public DateTime SomeDate { get; set; }
+}
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var obj1 = new MyClass()
+        {
+            Age = 1,
+            Name = "Tom",
+            SomeDate = DateTime.Now
+        };
+
+        TinyMapper.Bind<MyClass, MyClassDto>(config =>
+        {
+            //  config.Ignore(x => x.SomeDate);
+            config.Bind(s => s.SomeDate.ToLocalTime().DateTime, t => t.SomeDate);
+        }
+        );
+
+        var myClassDto = TinyMapper.Map<MyClassDto>(obj1);
+        //   myClassDto.SomeDate = obj1.SomeDate.DateTime;
+
+        Console.WriteLine(myClassDto.Name);
+        Console.ReadKey();
+
+    }
+}
